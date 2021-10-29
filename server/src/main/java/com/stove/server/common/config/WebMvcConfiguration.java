41d1c78d90bd2a3a936a1e9b78997e65fe0a59ce
koreaaerospaceU <zx811@naver.com>
2021-10-30 04:32:55 +0900

    feat: cors 설정

---
+++ server/src/main/java/com/stove/server/common/config/WebMvcConfiguration.java
@@ -0,0 +1,33 @@
+package com.stove.server.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by Minky on 2021-10-30
+ */
+
+/*
+ * Cors 설정
+ */
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .exposedHeaders("Location")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.OPTIONS.name()
+                );
+    }
+}
